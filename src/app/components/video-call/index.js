4629a663d089f3a8af86c9ac45e9f88dab7926b5
niilliiz <n.vatanara@mydoki.com>
2024-07-15 12:17:19 +0330

    initialize video call component

---
+++ src/app/components/video-call/index.js
@@ -0,0 +1,3 @@
+export default function VideoCall() {
+  return <div>video call</div>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
-import Image from "next/image";
+// "use client";
 import styles from "./page.module.css";
+import VideoCall from "@/app/components/video-call";
+
+// import AgoraRTC from "agora-rtc-sdk-ng";
+// AgoraRTC.setLogLevel(0);
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <VideoCall />
+    </main>
+  );
 }
