81a9828858f9059282ccd3fd953d54fe9331f643
niilliiz <n.vatanara@mydoki.com>
2024-07-17 18:01:35 +0330

    fail attempt

--- src/app/components/preview/index.js
+++ src/app/components/preview/index.js
@@ -1,11 +1,44 @@
-import styles from "./preview.module.css";
 import MediaControllers from "@/app/components/media-controllers";
+import styles from "./preview.module.css";
+import { useEffect, useRef, useState } from "react";
+import AgoraRTC, { useLocalCameraTrack, useLocalMicrophoneTrack } from "agora-rtc-react";
 
 export default function Preview() {
+  const [permissionsGranted, setPermissionsGranted] = useState(false);
+  const [localTracks, setLocalTracks] = useState([]);
+
+  const videoContainerRef = useRef(null);
+
+  const { localMicrophoneTrack } = useLocalMicrophoneTrack();
+  const { localCameraTrack } = useLocalCameraTrack();
+
+  // async function handlePermissions() {
+  //   try {
+  //     const [microphoneTrack, cameraTrack] = await AgoraRTC.createMicrophoneAndCameraTracks();
+  //     cameraTrack.play(videoContainerRef.current);
+  //     setLocalTracks([microphoneTrack, cameraTrack]);
+  //     console.log("mic", microphoneTrack);
+  //     console.log("cam", cameraTrack);
+  //   } catch (e) {
+  //     console.log("has error while getting permission");
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   console.log("here");
+  //   // get permission from user when comes to the preview page
+  //   handlePermissions();
+  //
+  //   return () => {
+  //     if (localTracks.length) {
+  //       localTracks.forEach(track => track.close());
+  //     }
+  //   };
+  // }, []);
   return (
     <div className={styles.previewContainer}>
       <div className={styles.videoContainer}>
-        <div className={styles.localVideoContainer}></div>
+        <div className={styles.localVideoContainer} ref={videoContainerRef}></div>
         <MediaControllers />
       </div>
       <div className={styles.infoContainer}>
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 import styles from "./page.module.css";
-import VideoCall from "@/app/components/video-call";
 import Client from "@/app/components/client";
 import Preview from "@/app/components/preview";
+import VideoCall from "@/app/components/video-call";
 
 // import AgoraRTC from "agora-rtc-sdk-ng";
 // AgoraRTC.setLogLevel(0);
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Client>
-        {/*<VideoCall />*/}
-        <Preview />
+        <VideoCall />
+        {/*<Preview />*/}
       </Client>
     </main>
   );
