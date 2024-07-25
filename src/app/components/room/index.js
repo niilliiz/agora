03679d218c6139a25923e50318fb8a7a8203029a
niilliiz <n.vatanara@mydoki.com>
2024-07-25 16:31:24 +0330

    feat: show videos

--- src/app/components/room/index.js
+++ src/app/components/room/index.js
@@ -1,26 +1,92 @@
 import styles from "./room.module.css";
-import { useIsConnected } from "agora-rtc-react";
+
+import {
+  useIsConnected,
+  useLocalCameraTrack,
+  useLocalMicrophoneTrack,
+  usePublish,
+  useRemoteAudioTracks,
+  useRemoteUsers,
+  useRemoteVideoTracks,
+} from "agora-rtc-react";
 import MediaController from "@/app/components/media-controller";
+import { useEffect, useRef } from "react";
 
 export default function Room({ micOn, cameraOn, setMicOn, setCameraOn, onLeave }) {
+  const remoteVideoRef = useRef(null);
+  const localVideoRef = useRef(null);
+
   const isConnected = useIsConnected();
-  console.log("isConnected Room", isConnected);
 
   function handleLeaveTheRoom() {
-    console.log("leave the room");
     onLeave();
   }
+
+  const remoteUsers = useRemoteUsers();
+  const publishedUsers = remoteUsers.filter(user => user.hasAudio || user.hasVideo);
+
+  const { videoTracks: remoteCameraTracks } = useRemoteVideoTracks(remoteUsers);
+  const { audioTracks: remoteMicrophoneTracks } = useRemoteAudioTracks(remoteUsers);
+  remoteMicrophoneTracks.map(track => track.play());
+
+  const { localMicrophoneTrack } = useLocalMicrophoneTrack(micOn);
+  const { localCameraTrack } = useLocalCameraTrack(cameraOn);
+  usePublish([localMicrophoneTrack, localCameraTrack]);
+
+  function handleToggleMic() {
+    if (micOn) {
+      setMicOn(false);
+      if (localMicrophoneTrack) {
+        localMicrophoneTrack.close();
+      }
+    } else {
+      setMicOn(true);
+      if (localMicrophoneTrack) {
+        localMicrophoneTrack.play();
+      }
+    }
+  }
+
+  function handleToggleCamera() {
+    if (cameraOn) {
+      setCameraOn(false);
+
+      if (localCameraTrack) {
+        localCameraTrack.stop();
+      }
+    } else {
+      setCameraOn(true);
+      if (localCameraTrack) {
+        localCameraTrack.play(localVideoRef.current);
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (remoteCameraTracks.length > 0 && remoteVideoRef.current) {
+      remoteCameraTracks[0].play(remoteVideoRef.current);
+    }
+  }, [remoteCameraTracks.length]);
+
+  useEffect(() => {
+    if (localCameraTrack && localVideoRef.current) {
+      localCameraTrack.play(localVideoRef.current);
+    }
+  }, [localCameraTrack]);
 
   return (
     <div className={styles.roomContainer}>
       <h1>Room</h1>
       <div className={styles.videoContainer}>
-        <div>videos remote or local</div>
+        <div className={styles.videoWrapper}>
+          <div className={styles.video} ref={remoteVideoRef} />
+          <div className={styles.video} ref={localVideoRef} />
+        </div>
         <MediaController
           micOn={micOn}
           cameraOn={cameraOn}
-          setMicOn={setMicOn}
-          setCameraOn={setCameraOn}
+          setMicOn={() => handleToggleMic()}
+          setCameraOn={() => handleToggleCamera()}
           onLeave={() => handleLeaveTheRoom()}
         />
       </div>
