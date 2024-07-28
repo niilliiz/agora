c51f1cec1bdb626e9523d6c172eed7f0a0eab929
niilliiz <n.vatanara@mydoki.com>
2024-07-28 17:49:41 +0330

    clean

--- src/hooks/useMediaControl.js
+++
@@ -1,8 +0,0 @@
-// import { useState } from "react";
-//
-// export default function useMediaControl() {
-//   const [micOn, setMicOn] = useState(false);
-//   const [cameraOn, setCameraOn] = useState(false);
-//
-//   return { micOn, setMicOn, cameraOn, setCameraOn };
-// }
