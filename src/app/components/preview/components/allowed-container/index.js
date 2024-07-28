134bb20ea6c149d60678d40bb4cfed266862b549
niilliiz <n.vatanara@mydoki.com>
2024-07-28 16:38:40 +0330

    refactor

--- src/app/components/preview/components/allowed-container/index.js
+++ src/app/components/preview/components/allowed-container/index.js
@@ -1,13 +1,62 @@
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useMemo, useRef, useState } from "react";
 import styles from "./allowed-container.module.css";
 import VideoContainer from "@/app/components/video-container";
 import MediaController from "@/app/components/media-controller";
+import { createMicrophoneAndCameraTracks } from "agora-rtc-sdk-ng/esm";
 
-function AllowedContainer({ hasPermission, onJoin }, ref) {
+function AllowedContainer({ hasPermission, setHasPermission, onJoin }) {
+  let isInitialRenderRef = useRef(true);
+  const videoContainerRef = useRef(null);
+
+  const [cameraOn, setCameraOn] = useState(false);
+  const [micOn, setMicOn] = useState(false);
+
+  const [localTracks, setLocalTracks] = useState({
+    cameraLocalTrack: null,
+    microphoneLocalTrack: null,
+  });
+
+  // const hasPermission = Object.values(localTracks).every(track => track !== null);
+
+  useMemo(() => {
+    setHasPermission(Object.values(localTracks).every(track => track !== null));
+  }, [localTracks]);
+
+  async function requestCameraAndMicrophonePermission() {
+    // todo: we force user to give permission for both medias, if we want to force them just to give for the one of them, we can use createCameraVideoTrack or createMicrophoneAudioTrack separately
+    try {
+      const [microphoneTrack, cameraTrack] = await createMicrophoneAndCameraTracks();
+      setLocalTracks(prevLocal => ({
+        ...prevLocal,
+        cameraLocalTrack: cameraTrack,
+        microphoneLocalTrack: microphoneTrack,
+      }));
+      setCameraOn(true);
+      setMicOn(true);
+      if (cameraTrack) {
+        cameraTrack.play(videoContainerRef.current);
+      }
+
+      if (microphoneTrack) {
+        microphoneTrack.play();
+      }
+    } catch (e) {
+      console.log(e, "User didn't give permission to either MICROPHONE or CAMERA");
+    }
+  }
+
+  useEffect(() => {
+    if (videoContainerRef.current && isInitialRenderRef.current) {
+      // Prevent from rendering the video element twice in DOM
+      isInitialRenderRef.current = false;
+
+      requestCameraAndMicrophonePermission();
+    }
+  }, []);
   return (
     <div className={styles.allowedContainer}>
       <div className={styles.mediaContainer}>
-        <VideoContainer ref={ref} />
+        <VideoContainer ref={videoContainerRef} />
         <MediaController />
       </div>
       <div className={styles.infoContainer}>
@@ -19,4 +68,4 @@ function AllowedContainer({ hasPermission, onJoin }, ref) {
   );
 }
 
-export default forwardRef(AllowedContainer);
+export default AllowedContainer;
--- src/app/components/preview/index.js
+++ src/app/components/preview/index.js
@@ -7,54 +7,15 @@ import NotAllowedContainer from "@/app/components/preview/components/not-allowed
 import AllowedContainer from "@/app/components/preview/components/allowed-container";
 
 export default function Preview({ onJoin }) {
-  let isInitialRenderRef = useRef(true);
-  const videoContainerRef = useRef(null);
-
-  const [cameraOn, setCameraOn] = useState(false);
-  const [micOn, setMicOn] = useState(false);
-
-  const [localTracks, setLocalTracks] = useState({
-    cameraLocalTrack: null,
-    microphoneLocalTrack: null,
-  });
-
-  const hasPermission = Object.values(localTracks).every(track => track !== null);
-
-  async function requestCameraPermission() {
-    // todo: we force user to give permission for both medias, if we want to force them just to give for the one of them, we can use createCameraVideoTrack or createMicrophoneAudioTrack separately
-    try {
-      const [microphoneTrack, cameraTrack] = await createMicrophoneAndCameraTracks();
-      setLocalTracks(prevLocal => ({
-        ...prevLocal,
-        cameraLocalTrack: cameraTrack,
-        microphoneLocalTrack: microphoneTrack,
-      }));
-      setCameraOn(true);
-      setMicOn(true);
-      if (cameraTrack) {
-        cameraTrack.play(videoContainerRef.current);
-      }
-
-      if (microphoneTrack) {
-        microphoneTrack.play();
-      }
-    } catch (e) {
-      console.log(e, "User didn't give permission to either MICROPHONE or CAMERA");
-    }
-  }
-
-  useEffect(() => {
-    if (videoContainerRef.current && isInitialRenderRef.current) {
-      // Prevent from rendering the video element twice in DOM
-      isInitialRenderRef.current = false;
-
-      requestCameraPermission();
-    }
-  }, []);
+  const [hasPermission, setHasPermission] = useState(false);
 
   return (
     <div className={styles.previewContainer}>
-      <AllowedContainer ref={videoContainerRef} onJoin={onJoin} hasPermission={hasPermission} />
+      <AllowedContainer
+        onJoin={onJoin}
+        hasPermission={hasPermission}
+        setHasPermission={value => setHasPermission(value)}
+      />
       {!hasPermission && <NotAllowedContainer />}
     </div>
   );
