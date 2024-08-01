396ce40f6ac10ece59f129a41fa042fd4d1c26d0
niilliiz <n.vatanara@mydoki.com>
2024-08-01 19:38:42 +0330

    feat: local in room ui

--- src/app/components/room/components/local-video-container/index.js
+++ src/app/components/room/components/local-video-container/index.js
@@ -1,5 +1,7 @@
 import styles from "./local-video-container.module.css";
 import { LocalAudioTrack, LocalVideoTrack, usePublish } from "agora-rtc-react";
+import CameraOffContainer from "@/app/components/camera-off";
+import UserState from "@/app/components/room/components/user-state";
 
 export default function LocalVideoContainer({
   cameraOn,
@@ -7,8 +9,11 @@ export default function LocalVideoContainer({
   localCameraTrack,
   localMicrophoneTrack,
 }) {
+  // connection-state-change,curState: RECONNECTING,revState: CONNECTED,reason: undefined
   return (
     <div className={styles.localVideoContainer}>
+      <CameraOffContainer />
+      <UserState name="local" micOn={micOn} />
       <LocalVideoTrack track={localCameraTrack} play={cameraOn} disabled={!cameraOn} />
       <LocalAudioTrack track={localMicrophoneTrack} play={micOn} disabled={!micOn} />
     </div>
