f23367554d962ca403915e6c2acbef242ce755db
niilliiz <n.vatanara@mydoki.com>
2024-08-04 11:50:40 +0330

    feat: performance changes

--- src/app/components/room/components/remote-video-container/index.js
+++ src/app/components/room/components/remote-video-container/index.js
@@ -5,28 +5,43 @@ import {
   useRemoteAudioTracks,
   useRemoteUsers,
   useRemoteVideoTracks,
+  useRTCClient,
 } from "agora-rtc-react";
 import UserState from "@/app/components/room/components/user-state";
 import CameraOffContainer from "@/app/components/camera-off";
+import { useMemo } from "react";
 
 export default function RemoteVideoContainer({ client = {} }) {
   const remoteUsers = useRemoteUsers();
+  const rtcClient = useRTCClient();
 
-  const hasRemoteUser = remoteUsers.length > 0;
+  const hasRemoteUser = useMemo(() => remoteUsers.length > 0, [remoteUsers.length]);
   const { videoTracks: remoteCameraTracks } = useRemoteVideoTracks(remoteUsers);
   const { audioTracks: remoteMicrophoneTracks } = useRemoteAudioTracks(remoteUsers);
 
   remoteMicrophoneTracks.map(track => track.play());
 
+  const userTracks = useMemo(() => {
+    if (!hasRemoteUser) return [];
+
+    return remoteUsers.map(user => {
+      const videoTrack = remoteCameraTracks.find(track => track.getUserId() === user.uid);
+      const audioTrack = remoteMicrophoneTracks.find(track => track.getUserId() === user.uid);
+
+      return {
+        user,
+        videoTrack,
+        audioTrack,
+      };
+    });
+  }, [remoteUsers, remoteCameraTracks, remoteMicrophoneTracks, hasRemoteUser]);
+
+  console.log("userTrack", userTracks);
+
   return (
     <div className={styles.remoteVideoContainer}>
-      {hasRemoteUser ? (
-        remoteUsers.map(user => {
-          const videoTrack = remoteCameraTracks.find(track => track.getUserId() === user.uid);
-          const audioTrack = remoteMicrophoneTracks.find(track => track.getUserId() === user.uid);
-
-          console.log("userV", videoTrack, user.hasVideo);
-          console.log("userA", audioTrack, user.hasAudio);
+      {userTracks.length > 0 ? (
+        userTracks.map(({ user, videoTrack, audioTrack }) => {
           return (
             <div key={user.uid}>
               <CameraOffContainer />
