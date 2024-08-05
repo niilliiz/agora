e5c52b1e194e117f8d9c1d9171283cf351c55f0a
niilliiz <n.vatanara@mydoki.com>
2024-08-05 23:07:13 +0330

    feat: too soon warning modal

--- src/app/components/room/index.js
+++ src/app/components/room/index.js
@@ -1,26 +1,65 @@
 import styles from "./room.module.css";
 
-import { useIsConnected, usePublish, useRTCClient } from "agora-rtc-react";
+import { useCurrentUID, useIsConnected, usePublish, useRTCClient } from "agora-rtc-react";
 import MediaController from "@/app/components/media-controller";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Container from "@/app/components/layout-components/container";
 import RemoteVideoContainer from "@/app/components/room/components/remote-video-container";
 import LocalVideoContainer from "@/app/components/room/components/local-video-container";
 import Timer from "@/app/components/timer";
 import useWindowWidth from "@/hooks/useWindowWidth";
+import { Limited_Second } from "@/utils/static-values";
+import EndNoticeModal from "@/app/components/end-notice-modal";
+import LeavingSoonErrorModal from "@/app/components/leaving-soon-error-modal";
+
+const startTime = 1722881194378;
+const User_Role = "doctor";
 
 export default function Room({ micOn, cameraOn, setMicOn, setCameraOn, onLeave, localTracks }) {
+  const [isLeavingSoonErrorModal, setIsLeavingSoonErrorModal] = useState(false);
+  const [isOpenNoticeModal, setIsOpenNoticeModal] = useState(false);
+
   const isConnected = useIsConnected();
   const client = useRTCClient();
 
+  const uid = useCurrentUID();
+
+  console.log(uid, "uid");
+
   const [width] = useWindowWidth();
+
+  const handleCloseLeavingSoonErrorModal = useCallback(() => setIsLeavingSoonErrorModal(false), []);
+
+  function handleOpenLeavingSoonErrorModal() {
+    setIsLeavingSoonErrorModal(true);
+  }
+
+  const handleCloseEndNoticeModal = useCallback(() => setIsOpenNoticeModal(false), []);
+
+  function handleOpenEndNoticeModal() {
+    setIsOpenNoticeModal(true);
+  }
 
   const { localCameraTrack, localMicrophoneTrack } = localTracks;
 
   usePublish([localMicrophoneTrack, localCameraTrack]);
 
   function handleLeaveTheRoom() {
-    onLeave();
+    const startTime = 1722886140;
+
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    const timeDifference = currentTime - startTime;
+
+    if (timeDifference < Limited_Second) {
+      handleOpenLeavingSoonErrorModal();
+    } else {
+      if (User_Role === "doctor") {
+        handleOpenEndNoticeModal();
+      } else {
+        window.location.href = "https://nexu.co";
+        onLeave();
+      }
+    }
   }
 
   useEffect(() => {
@@ -38,40 +77,47 @@ export default function Room({ micOn, cameraOn, setMicOn, setCameraOn, onLeave, 
   }, [isConnected, client, localCameraTrack, localMicrophoneTrack]);
 
   return (
-    <Container className={styles.container}>
-      <div className={styles.roomContainer}>
-        <div className={styles.videosWrapper}>
-          {isConnected && (
-            <>
-              {width <= 600 && (
-                <Timer
-                  className={styles.shortScreenTimer}
-                  autoStart={isConnected}
-                  remoteData="Remote Name"
+    <>
+      <Container className={styles.container}>
+        <div className={styles.roomContainer}>
+          <div className={styles.videosWrapper}>
+            {isConnected && (
+              <>
+                {width <= 600 && (
+                  <Timer
+                    className={styles.shortScreenTimer}
+                    autoStart={isConnected}
+                    remoteData="Remote Name"
+                  />
+                )}
+                <RemoteVideoContainer />
+
+                <LocalVideoContainer
+                  cameraOn={cameraOn}
+                  micOn={micOn}
+                  localCameraTrack={localCameraTrack}
+                  localMicrophoneTrack={localMicrophoneTrack}
                 />
-              )}
-              <RemoteVideoContainer />
+              </>
+            )}
+          </div>
 
-              <LocalVideoContainer
-                cameraOn={cameraOn}
-                micOn={micOn}
-                localCameraTrack={localCameraTrack}
-                localMicrophoneTrack={localMicrophoneTrack}
-              />
-            </>
-          )}
+          <MediaController
+            isJoined
+            micOn={micOn}
+            cameraOn={cameraOn}
+            setCameraOn={setCameraOn}
+            setMicOn={setMicOn}
+            onLeave={() => handleLeaveTheRoom()}
+          />
         </div>
-
-        <MediaController
-          isJoined
-          micOn={micOn}
-          cameraOn={cameraOn}
-          setCameraOn={setCameraOn}
-          setMicOn={setMicOn}
-          onLeave={() => handleLeaveTheRoom()}
-        />
-      </div>
-    </Container>
+      </Container>
+      <EndNoticeModal isOpen={isOpenNoticeModal} onClose={() => handleCloseEndNoticeModal()} />
+      <LeavingSoonErrorModal
+        isOpen={isLeavingSoonErrorModal}
+        onClose={() => handleCloseLeavingSoonErrorModal()}
+      />
+    </>
   );
 }
 
